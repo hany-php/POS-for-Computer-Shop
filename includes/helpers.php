fa89c9b04971109d7aceb933584279e6677a76ee
hany php <hany_hassan2000@hotmail.com>
2026-02-12 16:43:55 +0200

    لقد قمت بإصلاح الخطأ البرمجي (Fatal Error) الناتج عن تكرار تعريف دالة `generateCsrfToken()`.

--- includes/helpers.php
+++ includes/helpers.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * Helper Functions
@@ -23,21 +24,6 @@ function formatDateTimeAr($datetime) {
 function sanitize($input) {
     if ($input === null) return '';
     return htmlspecialchars(trim($input), ENT_QUOTES, 'UTF-8');
-}
-
-// CSRF Protection
-function generateCsrfToken() {
-    if (empty($_SESSION['csrf_token'])) {
-        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-    }
-    return $_SESSION['csrf_token'];
-}
-
-function verifyCsrfToken($token) {
-    if (!isset($_SESSION['csrf_token']) || $token !== $_SESSION['csrf_token']) {
-        return false;
-    }
-    return true;
 }
 
 // Flash message
